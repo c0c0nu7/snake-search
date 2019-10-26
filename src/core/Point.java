9289b881f058ed3dd1672fde5aad27bd80abcc55
Max Baumann <maximilian.baumann@faktorzehn.at>
2019-10-26 18:45:24 +0200

    initial commit

---
+++ src/core/Point.java
@@ -0,0 +1,20 @@
+/***************************************************************************************************
+ * Copyright (c) 2017 Faktor Zehn AG und andere.
+ * 
+ * Alle Rechte vorbehalten.
+ * 
+ * Mitwirkende: Faktor Zehn AG - initial API and implementation - http://www.faktorzehn.de
+ * 
+ **************************************************************************************************/
+
+package core;
+
+public class Point {
+    int x;
+    int y;
+    
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+}
