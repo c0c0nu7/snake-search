08f154b191fc1bed9cd1678270e1a1a1478d9c45
mbaumann <max.the.force@gmail.com>
2019-10-27 17:22:05 +0100

    refactoring, extracted code into methods, added test cases

--- src/core/Main.java
+++ src/core/Main.java
@@ -2,7 +2,8 @@ package core;
 
 public class Main {
     public static void main(String... args) {
-    	SnakeSearch.search("res/grid.txt", "INSURANCE");
+    	int search = SnakeSearch.search("res/grid.txt", "INSURANCE");
+    	System.out.println(search);
         return;
     }
 
--- test/core/SnakeSearchTest.java
+++ test/core/SnakeSearchTest.java
@@ -1,7 +1,8 @@
 package core;
 
-import static org.junit.Assert.*;
 import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
 import org.junit.Test;
 
 public class SnakeSearchTest {
@@ -9,6 +10,13 @@ public class SnakeSearchTest {
 	@Test
 	public void testSearch() {
 		assertThat(SnakeSearch.search("res/grid.txt", "INSURANCE"), is(1416));
+		assertThat(SnakeSearch.search("res/grid2.txt", "EINS"), is(4));
+		assertThat(SnakeSearch.search("res/grid3.txt", "ENE"), is(6));
+	}
+	
+	@Test(expected = IllegalArgumentException.class)
+	public void testEmptySearchString() {
+		SnakeSearch.search("res/grid.txt", "");
 	}
 	
 }
